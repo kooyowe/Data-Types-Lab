a0b81d57e7ed091ee65332396da798a8f796107f
koyowe <kevinoyowe@gmail.com>
2016-10-10 16:27:14 +0300

    Added Data Types Lab

---
+++ Data Types Lab.py
@@ -0,0 +1,28 @@
+"""
+Data Types Lab written by Kevin Oyowe
+October 10, 2016
+"""
+def data_type( dataType ):
+	"""
+	This function determines the type of data passed to it
+	and performs a certain operation as described for the 
+	specific data type
+	"""
+	if isinstance(dataType, basestring):
+		return len(dataType)
+	elif isinstance(dataType, bool):
+		return dataType
+	elif isinstance(dataType, int):
+		if dataType < 100:
+			return "less than 100"
+		elif dataType > 100:
+			return "more than 100"
+		else:
+			return "equal to 100"
+	elif isinstance(dataType, list):
+		try:
+			return dataType[2]
+		except IndexError:
+			return None
+	else:
+		return "no value"
